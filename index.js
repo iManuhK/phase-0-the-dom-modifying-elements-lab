637a28c76424cd726f37c3b742361fb4d6396c73
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-03-15 20:53:25 +0300

    Adds to index.js file DOM lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document
+main.id = "notMain"
+//const newHeader = document.getElementsByTag("h1")
+const newHeader = document.createElement("h1")
+newHeader.id="victory"
+newHeader.textContent = "Emmanuel is the champion"
